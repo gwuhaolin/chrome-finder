906efdaa569317deed2a2a53aa5eda882a5ba7b1
halwu(吴浩麟) <halwu@tencent.com>
2017-07-01 14:33:45 +0800

    tests: fix for node6

--- test/index.test.js
+++ test/index.test.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 const findChrome = require('../lib/index');
 
-describe('chrome-finder', () => {
+describe('chrome-finder', function () {
 
-  it('#findChrome()', async () => {
+  it('#findChrome()', function () {
     const chromePath = findChrome();
     fs.accessSync(chromePath, fs.constants.X_OK);
     console.log(chromePath);
