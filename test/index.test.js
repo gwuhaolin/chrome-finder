66bdb9558ddb80ca35c6098ddfaed6464467d88d
Daniel Andrei <daniel.andrei@ezugi.com>
2020-04-02 15:23:05 +0300

    fix catalina chrome detection
    set higher timeout for test

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,7 @@ const findChrome = require('../lib/index');
 describe('chrome-finder', function () {
 
   it('#findChrome()', function () {
+    this.timeout(10000);
     const chromePath = findChrome();
     fs.accessSync(chromePath, fs.constants.X_OK);
     console.log(chromePath);
